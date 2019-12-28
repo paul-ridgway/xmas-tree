d4c7256e802ceedd40e392c39705d3dd29c0c7bb
Paul Ridgway <paul@ridgway.io>
2019-12-28 16:49:09 +0000

    moved all to scenes

--- service/leds/scenes/chase2.ts
+++ service/leds/scenes/chase2.ts
@@ -8,9 +8,9 @@ export class Chase2 extends Scene {
     this.setDecay(2);
   }
 
-  valueFor = (led: number, offset: number, loops: number): RGB => {
-    let col = this.blank();
+  colForLoops = (loops: number): RGB => {
     const mod = loops % 5;
+    let col = this.blank();
     if (mod == 1) {
       this.setDecay(4);
       col = this.rgb(0.3, 1, 0.5);
@@ -23,15 +23,20 @@ export class Chase2 extends Scene {
     } else {
       col = this.rgb(0.8, 0.9, 1);
     }
+    return col;
+  }
+
+  valueFor = (led: number, offset: number, loops: number): RGB => {
+    
     if (led > 220) {
       this.setDecay(0);
-      return col;
+      return this.colForLoops(loops - 1);
     }
     this.setDecay(3);
     if (Math.round(offset * this.getLeds()) !== led) {
       return this.blank();
     }
-    return col;
+    return this.colForLoops(loops);;
   }
 
 }
